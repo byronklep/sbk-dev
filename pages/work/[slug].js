958fc2f4343724957cb57bf30125cceeb7ea13d9
Byron Klepper <byronklep@gmail.com>
2021-07-27 14:56:00 -0500

    Work slug page styling.

--- pages/work/[slug].js
+++ pages/work/[slug].js
@@ -2,6 +2,8 @@ import Image from 'next/image'
 import Link from 'next/link'
 import Layout from '../../components/Layout'
 import { getProject, getProjectSlugs, getProjectsWork } from '../../lib/data'
+import { FaExternalLinkAlt } from 'react-icons/fa'
+import { FaGitlab } from 'react-icons/fa'
 
 import he from 'he'
 import { MDXRemote } from 'next-mdx-remote'
@@ -74,46 +76,26 @@ export default function ProjectPage({ project, summary, fourProjects }) {
                   <div className="flex flex-col mt-6 space-y-3 lg:space-y-0 md:flex-row">
                     <Link href={`${project.links[0]}`} passHref>
                       <a target="_blank" rel="noreferrer">
-                        <button className="bg-white hover:text-gray-50 hover:bg-gray-800 focus:outline-none focus:ring-2  focus:ring-gray-400 text-gray-900 border rounded px-4 py-3 space-x-2 flex items-center justify-center flex-row ">
+                        <button className="bg-green-500 hover:text-gray-200 hover:bg-green-400 focus:outline-none focus:ring-2  focus:ring-gray-400 text-gray-900 border-indigo-900 rounded px-4 py-3 space-x-2 flex items-center justify-center flex-row ">
                           <div>
-                            <svg
-                              width="24"
-                              height="24"
-                              viewBox="0 0 24 24"
-                              fill="none"
-                              xmlns="http://www.w3.org/2000/svg">
-                              <path
-                                d="M12 2C6.475 2 2 6.475 2 12C1.99887 14.0993 2.65881 16.1456 3.88622 17.8487C5.11362 19.5517 6.84615 20.8251 8.838 21.488C9.338 21.575 9.525 21.275 9.525 21.012C9.525 20.775 9.512 19.988 9.512 19.15C7 19.613 6.35 18.538 6.15 17.975C6.037 17.687 5.55 16.8 5.125 16.562C4.775 16.375 4.275 15.912 5.112 15.9C5.9 15.887 6.462 16.625 6.65 16.925C7.55 18.437 8.988 18.012 9.562 17.75C9.65 17.1 9.912 16.663 10.2 16.413C7.975 16.163 5.65 15.3 5.65 11.475C5.65 10.387 6.037 9.488 6.675 8.787C6.575 8.537 6.225 7.512 6.775 6.137C6.775 6.137 7.612 5.875 9.525 7.163C10.3391 6.93706 11.1802 6.82334 12.025 6.825C12.875 6.825 13.725 6.937 14.525 7.162C16.437 5.862 17.275 6.138 17.275 6.138C17.825 7.513 17.475 8.538 17.375 8.788C18.012 9.488 18.4 10.375 18.4 11.475C18.4 15.313 16.063 16.163 13.838 16.413C14.2 16.725 14.513 17.325 14.513 18.263C14.513 19.6 14.5 20.675 14.5 21.013C14.5 21.275 14.688 21.587 15.188 21.487C17.173 20.8168 18.8979 19.541 20.1199 17.8391C21.3419 16.1373 21.9994 14.0951 22 12C22 6.475 17.525 2 12 2Z"
-                                fill="#111827"
-                              />
-                            </svg>
+                            <FaExternalLinkAlt />
                           </div>
 
                           <p className="text-base font-medium leading-none ">
-                            Go to Site
+                            Visit Site
                           </p>
                         </button>
                       </a>
                     </Link>
                     <Link href={`${project.links[1]}`} passHref>
                       <a target="_blank" rel="noreferrer">
-                        <button className="bg-white hover:text-gray-50 hover:bg-gray-800 focus:outline-none focus:ring-2  focus:ring-gray-400 text-gray-900 border rounded px-4 py-3 space-x-2 flex items-center lg:ml-3 justify-center flex-row ">
+                        <button className="bg-blue-500 hover:text-gray-50 hover:bg-blue-400 focus:outline-none focus:ring-2  focus:ring-gray-400 text-gray-900 border-blue-500 rounded px-4 py-3 space-x-2 flex items-center lg:ml-3 justify-center flex-row ">
                           <div>
-                            <svg
-                              width="24"
-                              height="24"
-                              viewBox="0 0 24 24"
-                              fill="none"
-                              xmlns="http://www.w3.org/2000/svg">
-                              <path
-                                d="M12 2C6.475 2 2 6.475 2 12C1.99887 14.0993 2.65881 16.1456 3.88622 17.8487C5.11362 19.5517 6.84615 20.8251 8.838 21.488C9.338 21.575 9.525 21.275 9.525 21.012C9.525 20.775 9.512 19.988 9.512 19.15C7 19.613 6.35 18.538 6.15 17.975C6.037 17.687 5.55 16.8 5.125 16.562C4.775 16.375 4.275 15.912 5.112 15.9C5.9 15.887 6.462 16.625 6.65 16.925C7.55 18.437 8.988 18.012 9.562 17.75C9.65 17.1 9.912 16.663 10.2 16.413C7.975 16.163 5.65 15.3 5.65 11.475C5.65 10.387 6.037 9.488 6.675 8.787C6.575 8.537 6.225 7.512 6.775 6.137C6.775 6.137 7.612 5.875 9.525 7.163C10.3391 6.93706 11.1802 6.82334 12.025 6.825C12.875 6.825 13.725 6.937 14.525 7.162C16.437 5.862 17.275 6.138 17.275 6.138C17.825 7.513 17.475 8.538 17.375 8.788C18.012 9.488 18.4 10.375 18.4 11.475C18.4 15.313 16.063 16.163 13.838 16.413C14.2 16.725 14.513 17.325 14.513 18.263C14.513 19.6 14.5 20.675 14.5 21.013C14.5 21.275 14.688 21.587 15.188 21.487C17.173 20.8168 18.8979 19.541 20.1199 17.8391C21.3419 16.1373 21.9994 14.0951 22 12C22 6.475 17.525 2 12 2Z"
-                                fill="#111827"
-                              />
-                            </svg>
+                            <FaGitlab />
                           </div>
 
                           <p className="text-base font-medium leading-none ">
-                            Go to Repo
+                            Visit Repo
                           </p>
                         </button>
                       </a>
@@ -161,11 +143,16 @@ export default function ProjectPage({ project, summary, fourProjects }) {
                 <div className="grid grid-cols-2 mt-6 gap-x-6 gap-y-6">
                   {project.techStack.map((tech, id) => (
                     <div key={id}>
-                      <p
+                      {/* <p
                         key={id}
                         className="text-base leading-none text-white capitalize">
                         {tech}
-                      </p>
+                      </p> */}
+                      <span
+                        key={id}
+                        className="inline-flex items-center px-3 py-1 rounded-md text-sm font-medium bg-purple-400 text-gray-900">
+                        {tech}
+                      </span>
                     </div>
                   ))}
                 </div>
@@ -179,11 +166,16 @@ export default function ProjectPage({ project, summary, fourProjects }) {
                 <div className="grid xl:grid-cols-4 sm:grid-cols-3 grid-cols-2 mt-6 gap-x-6 lg:gap-x-24 gap-y-6">
                   {project.infrastructure.map((i, id) => (
                     <div key={id}>
-                      <p
+                      {/* <p
                         key={id}
                         className="text-base leading-none text-white capitalize">
                         {i}
-                      </p>
+                      </p> */}
+                      <span
+                        key={id}
+                        className="inline-flex items-center px-3 py-1 rounded-md text-sm font-medium bg-yellow-400 text-gray-5a0">
+                        {i}
+                      </span>
                     </div>
                   ))}
                 </div>
@@ -191,15 +183,16 @@ export default function ProjectPage({ project, summary, fourProjects }) {
             </div>
           </div>
         </section>
-        <div className="mx-auto container py-16 px-6 lg:px-14 xl:px-0">
-          <div className="flex justify-start items-start flex-col">
+
+        <div className=" container py-16 px-6 lg:px-14  ">
+          <div className="flex justify-start items-start flex-col ">
             <div>
               <p className="text-2xl font-bold lg:leading-6 text-white">
                 Summary
               </p>
             </div>
-            <div className="ml-4 mt-8 xl:w-3/5">
-              <div className="text-base text-white list-disc xl:space-y-6 space-y-4">
+            <div className="ml-4 mt-8 w-3/4 md:w-full">
+              <div className="md:text-base text-white list-disc xl:space-y-6 space-y-4 text-sm overflow-scroll">
                 {<MDXRemote {...summary} />}
               </div>
             </div>
