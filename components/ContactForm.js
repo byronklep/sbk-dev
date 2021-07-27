ce19c1664e12f32a951765f9d3dd8e3d91cf96d3
Byron Klepper <byronklep@gmail.com>
2021-07-27 12:46:23 -0500

    Contact form styling - Home & Work pages.

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -18,7 +18,7 @@ export default function ContactForm({ id }) {
   }, [state])
 
   return (
-    <div className="min-h-screen md:w-1/2 bg-gray-900 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+    <div className="min-h-screen md:mx-auto md:w-3/4 bg-gray-900 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
         <div className="bg-gray-50 py-8 px-4 shadow sm:rounded-lg sm:px-10">
           <div className="sm:mx-auto sm:w-full sm:max-w-md">
