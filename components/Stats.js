a8c64a04a816a37031049bb664f94b12007269df
Byron Klepper <byronklep@gmail.com>
2021-07-28 00:05:54 -0500

    Animated stats.

--- components/Stats.js
+++ components/Stats.js
@@ -1,11 +1,14 @@
+import AnimatedNumber from 'animated-number-react'
+
 const stats = [
   { name: 'Projects', stat: '100+' },
   { name: 'Retention', stat: '88%' },
   { name: 'Client Retention', stat: '90%' },
-  { name: 'Users Impacted', stat: '5,000+' },
+  { name: 'Users Impacted', stat: '5000+' },
 ]
 
 export default function Stats() {
+  const formatValue = (value) => Math.trunc(value.toFixed(2))
   return (
     <div className="mx-auto">
       <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-2">
@@ -15,7 +18,14 @@ export default function Stats() {
             className="px-5 py-5 bg-white shadow rounded-lg overflow-hidden sm:p-6">
             <dt className="text-sm font-medium text-gray-500 ">{item.name}</dt>
             <dd className="mt-1 mr-5 p-2 text-2xl font-semibold text-gray-900">
-              {item.stat}
+              <AnimatedNumber value={item.stat} formatValue={formatValue} />{' '}
+              {item.name === 'Projects' ? (
+                <span>+</span>
+              ) : item.name === 'Users Impacted' ? (
+                <span>+</span>
+              ) : (
+                <span>%</span>
+              )}
             </dd>
           </div>
         ))}
